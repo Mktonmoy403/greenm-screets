dca1697da34ee032712538b5e6b5c98f1c1bfb85
mktonmoy <mktonmoy403@gmail.com>
2021-10-17 03:09:39 +0600

    slick slider and product page has done

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $('.slider-wrapper').slick({
+        arrows: true,
+        initialSlide: 0,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+        speed: 1500,
+        draggable: true,
+        dots: false,
+        pauseOnDotsHover: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        prevArrow:"<button type='button' class='slider-prev'><i class='fas fa-angle-left'></i></button>",
+        nextArrow:"<button type='button' class='slider-next'><i class='fas fa-angle-right'></i></button>",
+        responsive: [
+        {
+          breakpoint: 981,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 851,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 770,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        ]
+      });
+
+});
